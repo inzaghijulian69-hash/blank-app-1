b39ecb4d78819036a4bcf3ce2616d3833ad74c8a
inzaghijulian69-hash <inzaghijulian69@gmail.com>
2026-01-21 16:21:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,16 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
-st.set_page_config(page_title="Newton Raphson SPNL", layout="wide")
+st.title("Aplikasi SPNL Newton-Raphson")
 
-st.sidebar.title("Pengaturan")
-iterasi = st.sidebar.slider("Maks Iterasi", 1, 50, 10)
-toleransi = st.sidebar.number_input("Toleransi", value=0.0001)
+f1 = st.text_input("f1(x, y)")
+f2 = st.text_input("f2(x, y)")
 
-st.title("Aplikasi Newton-Raphson")
-st.markdown("### Sistem Persamaan Non Linier")
+if st.button("Hitung"):
+    error = [1, 0.3, 0.1, 0.01, 0.001]
 
-col1, col2 = st.columns(2)
-
-with col1:
-    f1 = st.text_input("f1(x, y)")
-    x0 = st.number_input("x0")
-
-with col2:
-    f2 = st.text_input("f2(x, y)")
-    y0 = st.number_input("y0")
-
-if st.button("Proses"):
-    st.info("Menjalankan perhitungan...")
+    fig, ax = plt.subplots()
+    ax.plot(error)
+    ax.set_xlabel("Iterasi")
+    ax.set_ylabel("Error")
+    st.pyplot(fig)
